4924a7684c86126e50eb705d05fe1e0a766a7df7
Olga <minaeva9@gmail.com>
2024-03-11 17:19:58 -0400

    footer

--- digital business card/src/App.jsx
+++ digital business card/src/App.jsx
@@ -2,13 +2,15 @@ import "./App.css";
 import Header from "./Header";
 import About from './About'
 import Interests from "./Interests";
+import Footer from "./Footer";
 
 function App() {
   return (
     <>
       <Header />
       < About />
-      <Interests/>
+      <Interests />
+      <Footer/>
     </>
   );
 }
---
+++ digital business card/src/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <i className="fa-brands fa-linkedin"></i>
+      <i className="fa-brands fa-facebook"></i>
+      <i className="fa-brands fa-square-instagram"></i>
+      <i className="fa-brands fa-square-github"></i>
+    </footer>
+  );
+}
